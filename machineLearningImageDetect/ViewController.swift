6aabc25495b5d3e45ab16da5d68f6a4a09399f9d
MRSgkce <mursidegokce985@gmail.com>
2025-03-15 23:40:56 +0300

    Initial Commit

---
+++ machineLearningImageDetect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  machineLearningImageDetect
+//
+//  Created by Mürşide Gökçe on 15.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

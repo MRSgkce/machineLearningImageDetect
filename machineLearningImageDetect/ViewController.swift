88ae6cd8ea53aa669cde0a0beb6b97eef7cdba4a
MRSgkce <mursidegokce985@gmail.com>
2025-03-16 13:49:18 +0300

    uygulamaya mobilnetv2 machine learning modeliyle resim tanımlama yapıldı.

--- machineLearningImageDetect/ViewController.swift
+++ machineLearningImageDetect/ViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageview: UIImageView!
+    var chosenImage = CIImage()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func imagePicker(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            imageview.image = selectedImage
+            if let ciImage = CIImage(image: selectedImage) {
+                chosenImage = ciImage
+                recognizeImage(image: chosenImage)
+            }
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func recognizeImage(image: CIImage) {
+        label.text = "Finding ..."
+        
+        guard let model = try? VNCoreMLModel(for: MobileNetV2().model) else {
+            label.text = "Model yüklenemedi"
+            return
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (vnRequest, error) in
+            guard let results = vnRequest.results as? [VNClassificationObservation], let topResult = results.first else {
+                DispatchQueue.main.async {
+                    self.label.text = "Tanımlama yapılamadı"
+                }
+                return
+            }
+            
+            let confidenceLevel = topResult.confidence * 100
+            let confidence = String(format: "%.2f", confidenceLevel)
+            
+            DispatchQueue.main.async {
+                self.label.text = "\(confidence)% it's \(topResult.identifier)"
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([request])
+            } catch {
+                DispatchQueue.main.async {
+                    self.label.text = "Resim işlenirken hata oluştu"
+                }
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
-
